6d0342e39dac497817b41d2d5a9cff5d25d7632f
Alberto López Sánchez <alopez@cells.es>
2021-01-26 17:55:49 +0100

    unfinished tango device server implementation

--- meadowlark_d5020/tango/server/meadowlark_d5020.py
+++ meadowlark_d5020/tango/server/meadowlark_d5020.py
@@ -7,47 +7,110 @@
 
 """Tango server class for Meadowlark_d5020"""
 
-import asyncio
-import urllib.parse
-
-from connio import connection_for_url
+import serial
 from tango import GreenMode
 from tango.server import Device, attribute, command, device_property
 
 import meadowlark_d5020.core
-
+from meadowlark_d5020.core import Waveform
 
 class Meadowlark_d5020(Device):
 
-    green_mode = GreenMode.Asyncio
-
     url = device_property(dtype=str)
 
-    async def init_device(self):
-        await super().init_device()
-        self.connection = connection_for_url(self.url, concurrency="async")
+    def init_device(self):
+        super().init_device()
+        self.connection = serial.connection_for_url(self.url)
         self.meadowlark_d5020 = meadowlark_d5020.core.Meadowlark_d5020(self.connection)
 
-    @attribute(dtype=str, label="ID")
-    def idn(self):
-        return self.meadowlark_d5020.get_idn()
+    ###########################################################################
 
-    @attribute(dtype=float, unit="bar", label="Pressure")
-    async def pressure(self):
-        # example processing the result
-        pressure = await self.meadowlark_d5020.get_pressure()
-        return pressure / 1000
+    @attribute(dtype=Waveform, label="Waveform pattern")
+    def waveform(self):
+        return self.meadowlark_d5020.waveform
 
-    @attribute(dtype=float, unit="bar", label="Pressure set point")
-    async def pressure_setpoint(self):
-        # example processing the result
-        setpoint = await self.meadowlark_d5020.get_pressure_setpoint()
-        return setpoint / 1000
+    @waveform.setter
+    def set_waveform(self, value):
+        self.meadowlark_d5020.waveform = value
+    
+    ###########################################################################
 
-    @pressure_setpoint.setter
-    def pressure_setpoint(self, value):
-        # example returning the coroutine back to tango
-        return self.meadowlark_d5020.get_pressure_setpoint(value * 1000)
+    @attribute(dtype=int, unit="mV", label="V1", min_value=0, max_value=10000,
+               doc="v1")
+    def v1(self):        
+        return self.meadowlark_d5020.v1
+
+    @v1.setter
+    def set_v1(self, value):        
+        self.meadowlark_d5020.v1 = value
+
+    ###########################################################################
+
+    @attribute(dtype=int, unit="mV", label="V2", min_value=0, max_value=10000,
+               doc="v2")
+    def v2(self):        
+        return self.meadowlark_d5020.v2
+
+    @v2.setter
+    def set_v2(self, value):        
+        self.meadowlark_d5020.v2 = value
+
+    ###########################################################################
+
+    @attribute(dtype=int, unit="ms", label="period", min_value=0, max_value=65535,
+               doc="period")
+    def period(self):        
+        return self.meadowlark_d5020.period
+
+    @period.setter
+    def set_period(self, value):        
+        self.meadowlark_d5020.period = value
+
+    ###########################################################################
+
+    @attribute(dtype=int, unit="degrees", label="phase", min_value=-360, max_value=360,
+               doc="phase")
+    def phase(self):        
+        return self.meadowlark_d5020.phase
+
+    @phase.setter
+    def set_phase(self, value):        
+        self.meadowlark_d5020.phase = value
+
+    ###########################################################################
+
+    @attribute(dtype=int, unit="%", label="duty cycle", min_value=0, max_value=100,
+               doc="duty cycle")
+    def duty_cycle(self):        
+        return self.meadowlark_d5020.duty_cycle
+
+    @duty_cycle.setter
+    def set_duty_cycle(self, value):        
+        self.meadowlark_d5020.duty_cycle = value
+
+    ###########################################################################
+
+    @attribute(dtype=int, unit="mV", label="Transient Nematic Effect Voltage", 
+               min_value=0, max_value=10000, doc="T.N.E Voltage")
+    def tne_voltage(self):        
+        return self.meadowlark_d5020.tne_voltage
+
+    @tne_voltage.setter
+    def set_tne_voltage(self, value):        
+        self.meadowlark_d5020.tne_voltage = value
+
+    ###########################################################################
+
+    @attribute(dtype=bool, label="External input", 
+               doc="Check if the external input is enabled")
+    def external_input(self):
+        return self.meadowlark_d5020.external_input
+
+    @command
+    def toggle_external_input(self):        
+        self.meadowlark_d5020.toggle_external_input()
+
+    ###########################################################################
 
     @command
     def turn_on(self):
